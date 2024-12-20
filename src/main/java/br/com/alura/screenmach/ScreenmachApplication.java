fb901fedc79d265d2f8c841c800affabd90d1695
CarlosBaldessari-Exe <marianobaldessari14@gmail.com>
2024-12-20 17:06:23 -0300

    A

--- src/main/java/br/com/alura/screenmach/ScreenmachApplication.java
+++ src/main/java/br/com/alura/screenmach/ScreenmachApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmach;
 
+import br.com.alura.screenmach.model.DadosEpisodios;
 import br.com.alura.screenmach.model.DadosSerie;
 import br.com.alura.screenmach.service.ConsumoApi;
 import br.com.alura.screenmach.service.ConverteDados;
@@ -21,11 +22,15 @@ public class ScreenmachApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		var consumoApi = new ConsumoApi(); //ConsumoApi al principio da lo mismo
-		var json= consumoApi.obterDados("https://www.omdbapi.com/?t=yellowstonex&apikey=6585022c");
+		var json= consumoApi.obterDados("https://www.omdbapi.com/?t=yellowstone&apikey=6585022c");
 		System.out.println(json);
 
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados= conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=yellowstone&season=1&episode=5&apikey=6585022c");
+		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+		System.out.println(dadosEpisodios);
+
 	}
 }
